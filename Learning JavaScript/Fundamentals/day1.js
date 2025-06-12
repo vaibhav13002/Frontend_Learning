662be5e9b4f609285a81d3ec8d8e554691416208
Your Name <your@email.com>
2025-06-12 09:28:38 +0530

    day1

---
+++ Learning JavaScript/Fundamentals/day1.js
@@ -0,0 +1,21 @@
+let PI = 3.14;
+// console.log(PI);
+PI = "Vaibhav";
+// console.log(PI);
+
+// type of , Variables, null, Undefined
+//remember nul is a Object in JAVASCRIPT, this is bug which is not resolved till date for legacy resons.
+let variable1='';
+let variable2 = 0;
+let variable3=null;
+
+console.log(variable1);
+console.log(variable2);
+console.log(variable3);
+console.log("Now Type Of");
+console.log(typeof variable1);
+console.log(typeof variable2);
+console.log(typeof variable3);
+
+
+
