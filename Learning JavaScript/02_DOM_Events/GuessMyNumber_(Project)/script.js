f96f624d5a45d2cf1ee33525be1c41d04a465add
Your Name <your@email.com>
2025-06-16 14:51:46 +0530

    guesMyNumber project

---
+++ Learning JavaScript/02_DOM_Events/GuessMyNumber_(Project)/script.js
@@ -0,0 +1,71 @@
+"usestrict";
+
+// console.log(document.querySelector(".message").textContent);
+// document.querySelector(".message").textContent = "Changing Text data";
+// document.querySelector(".guess").value = 11;
+// console.log(document.querySelector(".guess").value);  // read karega jo text box area me hoga
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highscore = 0;
+// document.querySelector(".number").textContent = secretNumber;
+
+//fucntion to display message
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  // console.log(guess , typeof guess);
+
+  if (!guess) {
+    // document.querySelector(".message").textContent = "⚠️ No Number";
+    displayMessage("⚠️ No Number");
+  } else if (guess === secretNumber) {
+    // document.querySelector(".message").textContent = "Correct Answer 🎉";
+    displayMessage("Correct Answer 🎉");
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").textContent = secretNumber;
+    highscore = Math.max(highscore, score);
+    document.querySelector(".highscore").textContent = highscore;
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      // document.querySelector(".message").textContent =
+      //   guess > secretNumber ? "📈Too high!" : "📉Too low!";
+      displayMessage(guess > secretNumber ? "📈Too high!" : "📉Too low!");
+    } else {
+      // document.querySelector(".message").textContent = "🥺You lost the game!";
+      displayMessage("🥺You lost the game!");
+    }
+    score--;
+    document.querySelector(".score").textContent = score;
+  }
+
+  // else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector(".message").textContent = "📈Too high!";
+  //   } else {
+  //     document.querySelector(".message").textContent = "🥺You lost the game!";
+  //   }
+  //   score--;
+  //   document.querySelector(".score").textContent = score;
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector(".message").textContent = "📉Too low!";
+  //   } else {
+  //     document.querySelector(".message").textContent = "🥺You lost the game!";
+  //   }
+  //   score--;
+  //   document.querySelector(".score").textContent = score;
+  // }
+});
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".score").textContent = "20";
+  document.querySelector(".guess").value = "";
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector(".number").textContent = "?";
+  // document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
---
+++ Learning JavaScript/02_DOM_Events/day4.js
@@ -0,0 +1,3 @@
+// document.querySelector('selector')
+/* Ye method HTML ke andar se pehla element return karta hai jo selector match karta hai (just like CSS selector).
+➡️ Sirf pehla match return karega. */
